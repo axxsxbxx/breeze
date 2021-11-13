5df6f0a374baff64cafa568fb66d36cb4c8bec11
DESKTOP-KVCQHCD\multicampus <jm9521@hanmail.net>
2021-11-14 01:19:27 +0900

    ✨ feat: Add login guard

--- breeze/frontend/src/router/index.js
+++ breeze/frontend/src/router/index.js
@@ -11,29 +11,44 @@ import LoginRedirect from '../views/LoginRedirect.vue'
 import NotFound from '@/views/NotFound.vue'
 import authApi from '@/api/auth.js'
 import store from "@/store"
+import Swal from 'sweetalert2'
 
 
 Vue.use(VueRouter)
 
-const checkToken =  () => async (to, from, next) => {
-  console.log(from, '어디서 왔는가')
-  if (from.name !== 'LoginRedirect') {
-    const userId = store.getters.getUserId
-    const isExpired = await authApi.check(userId)
-    console.log(isExpired, '한 페이지에 너무 오래 머물렀니')
-    if (isExpired) {
-      const response = await authApi.logout(userId)
-      if (response == 'success') {
-        await store.dispatch('removeUser')
-        sessionStorage.clear()
-        next('/')
+const checkLoginToken =  () => async (to, from, next) => {
+  const isLoggedIn = store.getters.getUserId
+  //로그인 상태 처리
+  if (isLoggedIn) {
+    //토큰 만료 여부 체크 (약속 만드는 과정 제외)
+    if (to.name == 'EnterInfo' || to.name == 'OneClick' || (to.name =='Home' && from.name !== 'LoginRedirect')) {
+      const isExpired = await authApi.check(isLoggedIn)
+      if(isExpired) {
+        const response = await authApi.logout(isLoggedIn)
+        if (response == 'success') {
+          await store.dispatch('removeUser')
+          sessionStorage.clear()
+          next('/')
+        }
+      } else {
+        return next()
       }
-    }
-    else {
+    } else {
       return next()
     }
+  // 비로그인 상태 처리
+  } else {
+    Swal.fire({
+      html: '<b>로그인이 필요한 서비스입니다!</b>',
+      imageUrl: 'https://ifh.cc/g/bYAc4j.png',
+      imageWidth: 170,
+      imageHeight: 170,
+      imageAlt: 'Logo image',
+      showConfirmButton: false,
+      showCloseButton: true,
+    })
+    next('/')
   }
-  return next()
 }
 
 const routes = [
@@ -46,37 +61,36 @@ const routes = [
     path: '/home',
     name: 'Home',
     component: Home,
-    beforeEnter: checkToken()
+    beforeEnter: checkLoginToken()
   },
   {
     path: '/enterinfo',
     name: 'EnterInfo',
     component: EnterInfo,
-    beforeEnter: checkToken()
+    beforeEnter: checkLoginToken()
   },
   {
     path: '/findmiddle',
     name: 'FindMiddle',
     component: FindMiddle,
-    // beforeEnter: checkToken()
+    beforeEnter: checkLoginToken()
   },
   {
     path: '/findplace',
     name: 'FindPlace',
     component: FindPlace,
-    // beforeEnter: checkToken()
+    beforeEnter: checkLoginToken()
   },
   {
     path: '/makeappointment/:secretCode',
     name: 'MakeAppointment',
     component: MakeAppointment,
-     // beforeEnter: checkToken()
   },
   {
     path: '/oneclick',
     name: 'OneClick',
     component: OneClick,
-    beforeEnter: checkToken()
+    beforeEnter: checkLoginToken()
   },
   {
     path: '/oauth/kakao/callback',
@@ -86,11 +100,13 @@ const routes = [
   {
     path: '/404',
     name: 'NotFound',
-    component: NotFound
+    component: NotFound,
+    beforeEnter: checkLoginToken()
   },
   { path: '*',
     name: 'NotFound',
-    component: NotFound
+    component: NotFound,
+    beforeEnter: checkLoginToken()
   },
 ]
 
